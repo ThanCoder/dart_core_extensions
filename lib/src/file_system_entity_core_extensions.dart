16726f8e6b6e76759e867301ed74090a8fbd73e6
thancoder <thancoder2019@gmail.com>
2026-05-17 12:25:03 +0630

    update

--- lib/src/file_system_entity_core_extensions.dart
+++ lib/src/file_system_entity_core_extensions.dart
@@ -1,29 +1,39 @@
 import 'dart:io';
 
-import 'package:dart_core_extensions/src/path_core_extensions.dart';
+import 'package:dart_core_extensions/dart_core_extensions.dart';
 
+/// file entity special class
 extension FileSystemEntityCoreExtensions on FileSystemEntity {
+  /// file name
   String getName({bool withExt = true}) {
     return path.getName(withExt: withExt);
   }
 
+  /// ext name
   String get extName {
     return path.extName;
   }
 
+  /// is folder
   bool get isDirectory {
     return statSync().type == FileSystemEntityType.directory;
   }
 
+  /// is file
   bool get isFile {
     return statSync().type == FileSystemEntityType.file;
   }
 
+  /// folder
   Directory get getDirectory => Directory(path);
 
+  /// folder
   Directory get directory => Directory(path);
 
+  /// file
   File get getFile => File(path);
+
+  /// file
   File get file => File(path);
 
   /// The size of the file system object.
@@ -31,6 +41,12 @@ extension FileSystemEntityCoreExtensions on FileSystemEntity {
     return statSync().size;
   }
 
+  /// format file size
+  String fileSizeLabel({int asFixed = 2}) {
+    return size.fileSizeLabel(asFixed: asFixed);
+  }
+
+  /// file size
   Future<int> sizeAsync() async {
     return (await stat()).size;
   }
